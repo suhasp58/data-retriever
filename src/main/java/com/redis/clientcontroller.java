90e6ada35f66c78b6be99a867cc830c375502e74
suhasp58 <52521805+suhasp58@users.noreply.github.com>
2019-08-01 16:53:11 +0530

    Update clientcontroller.java

--- src/main/java/com/redis/clientcontroller.java
+++ src/main/java/com/redis/clientcontroller.java
@@ -1,12 +1,14 @@
 package com.redis;
 
-import java.io.InputStream;
-import java.util.Properties;
+//import java.io.InputStream;
+//import java.util.Properties;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import redis.clients.jedis.Jedis;
@@ -14,17 +16,19 @@ import redis.clients.jedis.Jedis;
 @RestController
 @RequestMapping("server")
 public class clientcontroller {
- public static String datainport,key,value;
+ public static String datainport,key;
 	@RequestMapping("/getData")
-	public String server1(@RequestBody String data) throws Exception {
-		Properties prop = new Properties();
-		InputStream input = clientcontroller.class.getClassLoader().getResourceAsStream("application.properties");
-		prop.load(input);
-
-	  JSONObject obj = new JSONObject(data);
-		String server = obj.getString("server");
-		int port = obj.getInt("port");
-		Jedis jedis = new Jedis("localhost",6379);
+	public String server1(@RequestParam String server , @RequestParam int port ) throws Exception {
+		
+		//InputStream input = clientcontroller.class.getClassLoader().getResourceAsStream("application.properties");
+		
+        System.out.println(server);
+        System.out.println(port);
+	 //JSONObject obj = new JSONObject(data);
+		//String server = obj.getString("server");
+		// int port=(int) Long.parseLong(port);
+		//int port = obj.getInt("port");
+		Jedis jedis = new Jedis("192.168.195.233", 6379);
          try {
          String serverstring =jedis.get(server);
          JSONObject object = new JSONObject(serverstring);
@@ -33,13 +37,14 @@ public class clientcontroller {
 		  int key = n1.getJSONObject(i).getInt("port");
 		 if( port == key ) {
 		 JSONObject value =n1.getJSONObject(i); 
-		 datainport=value.toString();
+		String output=value.toString();
+		  datainport="Server : "+server+".   Data : "+output;
 		 //System.out.println(value);
 		 }}
 		 }finally {
 			 jedis.close();
 		 }
 		return datainport;
-	
 		 }
+	
 }
